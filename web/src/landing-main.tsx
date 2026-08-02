f338e7b6be58faadf5b2240a7afe42c8b247a15a
oexecutor <oexecutor26@gmail.com>
2026-08-02 17:18:25 -0300

    style(landing): carregar refinamento tipográfico

--- web/src/landing-main.tsx
+++ web/src/landing-main.tsx
@@ -4,6 +4,7 @@ import { Landing } from "./Landing";
 import { registerPwa } from "./pwa";
 import "./styles.css";
 import "./entry.css";
+import "./entry-refine.css";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Root element not found");
